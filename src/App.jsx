3c604d521250035577a589781ca09bd01551812b
andreanjunior <git@stackblitz.com>
2025-01-22 10:12:28 -0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Board from './components/Board';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="App">
+      <h1> Jogo da Velha </h1>
+      <Board />
+    </div>
+  );
+}
+
+export default App;
